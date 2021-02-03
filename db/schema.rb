f2fc1bc37b24857316a944fe12e7266a926862e2
David Tran <davidttemail@gmail.com>
2021-02-02 18:41:31 -0700

    Add migration, username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_230722) do
+ActiveRecord::Schema.define(version: 2021_02_02_171750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_230722) do
     t.string "visitor_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
