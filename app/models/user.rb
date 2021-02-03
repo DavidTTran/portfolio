1b2f531545b6159727112f8d6bcdcfb1dd440718
David Tran <davidttemail@gmail.com>
2021-02-02 18:39:45 -0700

    Add username helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_many :visits, class_name: "Ahoy::Visit"
+
+  before_save do
+    username_generator
+  end
+
+  private
+
+  def username_generator
+    self.username = "visitor-#{srand.to_s.last(4)}"
+    return if User.where(username: self.username).empty?
+    username_generator
+  end
 end
