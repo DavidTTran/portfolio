db334b35094eb82cbd6ccda9aa89a6e08ac24e37
David Tran <davidttemail@gmail.com>
2021-05-11 14:12:04 -0600

    Checks current_visit.visitor_id for unique ids

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    require "pry"; binding.pry
+    if User.where(visitor_id: current_visit.visitor_token).empty?
+      current_user = User.create(visitor_id: current_visit.visitor_token)
+    else
+      current_user = User.find_by_visitor_id(current_visit.visitor_token)
+    end
+    @visitor = Visitor.new(current_user)
   end
 end
