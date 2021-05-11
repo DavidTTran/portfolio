bc50027687623a272a7f2251353e9e6cd537b0da
David Tran <davidttemail@gmail.com>
2021-05-11 17:20:21 -0600

    Additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/about", to: "about#index"
+  get "/openai", to: "openai#index"
+  get "/contact", to: "contact#index"
 end
