f6a804b81c76115c95da997a428c130bbe7d7f4c
David Tran <davidttemail@gmail.com>
2021-05-11 14:11:29 -0600

    Users generate unique username on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :visits, class_name: "Ahoy::Visit"
+
+  before_create :generate_username
+
+  def generate_username
+    self.username = "visitor-#{srand.to_s.last(4)}"
+    return if User.where(username: self.username).empty?
+    generate_username
+  end
 end
