5ec2672b6b7dcdfd0852232653b6e6a9662f1eaa
David Tran <davidttemail@gmail.com>
2021-05-15 17:29:51 -0600

    Add comments page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
-
   get "/about", to: "about#index"
   get "/openai", to: "openai#index"
   get "/contact", to: "contact#index"
+  get "/comments", to: "comments#index"
+
+  get '/auth/:provider/callback', to: 'welcome#update'
 end
