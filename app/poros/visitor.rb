871f7dc9387666e7f1f0a6296742b30803e38764
David Tran <davidttemail@gmail.com>
2021-05-15 17:31:38 -0600

    Add oauth method to visitors

--- app/poros/visitor.rb
+++ app/poros/visitor.rb
@@ -1,11 +1,15 @@
 class Visitor
   attr_reader :current_user
 
-  def initialize(current_user)
+  def initialize(current_user = nil)
     @current_user = current_user
   end
 
   def count_of_visitors
     User.all.size
+  end
+
+  def oauth(params)
+    GithubService.call(params)
   end
 end
