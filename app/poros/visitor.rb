e771dc4903728eda6b6a6df2ed4054544bb92e42
David Tran <davidttemail@gmail.com>
2021-05-18 16:23:19 -0600

    Refactor

--- app/poros/visitor.rb
+++ app/poros/visitor.rb
@@ -10,6 +10,13 @@ class Visitor
   end
 
   def oauth(params)
-    GithubService.new.call(params)
+    data = GithubService.new.call(params)
+    update_visitor(data)
+  end
+
+  private
+
+  def update_visitor(data)
+    current_user.update(username: data[:login], avatar_url: data[:avatar_url])
   end
 end
