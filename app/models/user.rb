3d3b62fe6437b2a3612a3b9794b38bc06fae257a
David Tran <davidttemail@gmail.com>
2021-05-18 01:01:28 -0600

    Update default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   has_many :visits, class_name: "Ahoy::Visit"
 
-  before_create :generate_username
+  before_create :generate_username, :default_avatar
 
   def generate_username
     self.username = "visitor-#{srand.to_s.last(4)}"
     return if User.where(username: self.username).empty?
     generate_username
+  end
+
+  def default_avatar
+    self.avatar_url = "https://imgur.com/C91oByQ"
   end
 end
