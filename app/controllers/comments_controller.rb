042928d36ab2e59b0775c779e1d6bdc13a303af8
David Tran <davidttemail@gmail.com>
2021-05-18 16:24:35 -0600

    Write create comments method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.all
+    @comments = Comment.all.order(created_at: :desc)
+  end
+
+  def create
+    params = comment_params
+    Comment.create(user_id: current_user.id, body: params[:body])
+    redirect_to "/comments"
+  end
+
+  private
+
+  def comment_params
+    params.permit(:body)
   end
 end
